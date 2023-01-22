12fe06515adb4fc86be7a46b2e6a4ee223aa94bd
Tailwind Mechanics <shea@tailwindmechanics.com>
2023-01-22 18:50:26 +0000

    Built runtime UI for conversation with AI

--- Assets/_Gpt-3/Modules/OpenAI/Scripts/Internal/Behaviours/OpenAI.cs
+++ Assets/_Gpt-3/Modules/OpenAI/Scripts/Internal/Behaviours/OpenAI.cs
@@ -2,6 +2,8 @@
 using JetBrains.Annotations;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using OpenAI_API;
+using TMPro;
 
 using Modules.ModulesSystem.External.DataObjects;
 using Modules.OpenAI.External.DataObjects;
@@ -16,22 +18,14 @@ namespace Modules.OpenAI.Internal.Behaviours
         const bool disabled = true;
         [FoldoutGroup("References"), GUIColor("$GetModuleColor"), SerializeField]
         OpenApiCredentialsSo openApiCredentialsSo;
-        [FoldoutGroup("Tooling"), TextArea, SerializeField]
-        string input;
-        [FoldoutGroup("Tooling"), Button(ButtonSizes.Medium)]
-        void DoCall () => Call();
-        [FoldoutGroup("Tooling"), PropertyOrder(1), TextArea, SerializeField]
-        string output;
-
-        async Task Call ()
-        {
-            LogMessage("Ask", input);
-            var api = new OpenAI_API.OpenAIAPI(openApiCredentialsSo.ApiKey);
-            var temp = Random.Range(0.1f, 1.0f);
-            var result = await api.Completions.CreateCompletionAsync(input, temperature: temp);
-            output = result.ToString().Trim();
-            LogMessage("Answer", output, 1);
-        }
+        [FoldoutGroup("Tooling"), SerializeField]
+        TMP_InputField inputBox;
+        [FoldoutGroup("Tooling"), SerializeField]
+        Transform repliesParent;
+        [FoldoutGroup("Tooling"), SerializeField]
+        SpawnableChatReply humanReply;
+        [FoldoutGroup("Tooling"), SerializeField]
+        SpawnableChatReply aiReply;
         #endregion Fields
 
 
@@ -41,5 +35,52 @@ namespace Modules.OpenAI.Internal.Behaviours
         public OutputVo<IOpenAIOutput> outputVo;
         void Awake() => outputVo = new OutputVo<IOpenAIOutput>(this);
         #endregion IO Setup
+
+        void OnEnable()
+            => inputBox.onSubmit.AddListener(body => OnSend("", body));
+        void OnDisable()
+        {
+            inputBox.onSubmit.RemoveAllListeners();
+            var children = repliesParent.GetComponentsInChildren<SpawnableChatReply>();
+            foreach (var child in children)
+            {
+                Destroy(child);
+            }
+        }
+
+        async Task Call (string input)
+        {
+            var api = new OpenAIAPI(openApiCredentialsSo.ApiKey);
+            var request = new CompletionRequest
+            (
+                input,
+                Model.DavinciText,
+                200,
+                0.5,
+                presencePenalty: 0.1,
+                frequencyPenalty: 0.1
+            );
+
+            var reply = Instantiate(aiReply, repliesParent);
+            await foreach (var token in api.Completions.StreamCompletionEnumerableAsync(request))
+            {
+                reply.SetText("", token.ToString());
+            }
+
+            ToggleInputBox(true);
+        }
+
+        void OnSend (string title, string body)
+        {
+            var input = inputBox.text;
+            inputBox.text = "";
+            ToggleInputBox(false);
+            var reply = Instantiate(humanReply, repliesParent);
+            reply.SetText(title, body);
+            Call(input);
+        }
+
+        void ToggleInputBox (bool isInteractable)
+            => inputBox.interactable = isInteractable;
     }
 }
---
+++ Assets/_Gpt-3/Modules/OpenAI/Scripts/Internal/Behaviours/SpawnableChatReply.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using TMPro;
+
+
+namespace Modules.OpenAI.Internal.Behaviours
+{
+	public class SpawnableChatReply : MonoBehaviour
+	{
+		[SerializeField] TextMeshProUGUI titleText;
+		[SerializeField] TextMeshProUGUI bodyText;
+
+		public void SetText (string title, string body)
+		{
+			titleText.text = title;
+			bodyText.text += body;
+		}
+	}
+}
