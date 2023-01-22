bc94c592cde5cc5a1fe618c2509cb68c1304dd45
Tailwind Mechanics <shea@tailwindmechanics.com>
2023-01-22 15:53:34 +0000

    Open AI api added

---
+++ Assets/_Gpt-3/Modules/OpenAI-API-dotnet-master/OpenAI_Tests/SearchEndpointTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenAI_API;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace OpenAI_Tests
+{
+	public class SearchEndpointTests
+	{
+		[SetUp]
+		public void Setup()
+		{
+			OpenAI_API.APIAuthentication.Default = new OpenAI_API.APIAuthentication(Environment.GetEnvironmentVariable("TEST_OPENAI_SECRET_KEY"));
+		}
+
+        /*
+		[Test]
+		public void TestBasicSearch()
+		{
+			var api = new OpenAI_API.OpenAIAPI();
+
+			Assert.IsNotNull(api.Search);
+
+			var result = api.Search.GetBestMatchAsync("Washington DC", "Canada", "China", "USA", "Spain").Result;
+			Assert.IsNotNull(result);
+			Assert.AreEqual("USA", result);
+		}
+		*/
+        // TODO: Maybe implement a shim based on https://github.com/openai/openai-cookbook/blob/main/transition_guides_for_deprecated_API_endpoints/search_functionality_example.py ?
+
+    }
+}
