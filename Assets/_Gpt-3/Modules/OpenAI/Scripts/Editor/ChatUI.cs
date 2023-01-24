f6533d20b229a2411390231be038210509746b69
Tailwind Mechanics <shea@tailwindmechanics.com>
2023-01-24 23:29:59 +0000

    UIToolkit chat editor window blocked out

---
+++ Assets/_Gpt-3/Modules/OpenAI/Scripts/Editor/ChatUI.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Threading.Tasks;
+using Label = UnityEngine.UIElements.Label;
+using UnityEngine.UIElements;
+using UnityEditor;
+using UnityEngine;
+
+
+namespace Modules.OpenAI.Editor
+{
+    public class ChatUI : EditorWindow
+    {
+        const string uxmlPath   = "Assets/_Gpt-3/Modules/OpenAI/UI/OpenAI_visualTree.uxml";
+        const string ussPath    = "Assets/_Gpt-3/Modules/OpenAI/UI/OpenAI_uss.uss";
+
+        const string inputBoxTextFieldName  = "inputBox_textField";
+        const string chatBoxScrollViewName  = "chatBox_scrollView";
+        const string sendButtonName         = "send_button";
+
+        ScrollView chatBoxScrollView;
+        TextField inputBoxTextField;
+        VisualElement root;
+        Button sendButton;
+
+        // todo
+        // Learn how to add a pre-made visual element
+            // with a title and body and uss already applied
+            // So all that's needed is to pass in the name of who sent the reply
+        // Learn how to capture keyboard input
+            // So return button sends reply, then remove send button
+        // Make the reply box grow in size as the reply length increases
+        // Learn how to setup same functionality at runtime
+            // so it is clear how and how to abstract the chat system
+
+        [MenuItem("Testing/Show Window")]
+        public static void ShowWindow ()
+        {
+            var window = GetWindow<ChatUI>();
+            window.titleContent = new GUIContent("Chat");
+            window.minSize = new Vector2(100, 100);
+        }
+
+        void CreateGUI()
+        {
+            root = rootVisualElement;
+            var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(uxmlPath);
+            root.Add(visualTree.Instantiate());
+
+            var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>(ussPath);
+            root.styleSheets.Add(styleSheet);
+
+            inputBoxTextField   = root.Q<TextField>(inputBoxTextFieldName);
+            chatBoxScrollView   = root.Q<ScrollView>(chatBoxScrollViewName);
+            sendButton          = root.Q<Button>(sendButtonName);
+            sendButton.clicked += () =>
+            {
+                chatBoxScrollView.Add(new Label(inputBoxTextField.text));
+                inputBoxTextField.SetValueWithoutNotify("");
+            };
+        }
+    }
+}
